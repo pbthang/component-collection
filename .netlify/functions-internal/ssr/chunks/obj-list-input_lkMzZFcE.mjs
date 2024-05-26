fa444b590e38dd7ac883118616b7097ffe17217c
Thang Pham <bathang02@gmail.com>
2024-05-26 17:19:46 +0800

    fix deployment error

--- .netlify/functions-internal/ssr/chunks/obj-list-input_lkMzZFcE.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./obj-list-input_D71Q80IV.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
