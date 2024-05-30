f4f08edeb48846debfbc9471e48e33da3c007cb8
Thang Pham <bathang02@gmail.com>
2024-05-30 13:35:46 +0800

    add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,64 +4,51 @@ import tailwind from "@astrojs/tailwind";
 import liveCode from "astro-live-code";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
-
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "pbthang",
-      social: {
-        email: "mailto:bathang02@gmail.com",
-        github: "https://github.com/pbthang",
-        linkedin: "https://www.linkedin.com/in/pbthang/",
-      },
-      favicon: "/favicon.svg",
-      head: [
-        {
-          tag: "link",
-          attrs: {
-            rel: "sitemap",
-            href: "/sitemap-index.xml",
-          },
-        },
-      ],
-      sidebar: [
-        {
-          label: "Components",
-          items: [
-            {
-              label: "Overview",
-              link: "/components",
-            },
-            {
-              label: "Hero",
-              autogenerate: {
-                directory: "/components/hero",
-              },
-            },
-            {
-              label: "Shadcn/ui",
-              autogenerate: {
-                directory: "/components/shadcn",
-              },
-            },
-          ],
-        },
-      ],
-      customCss: ["./src/tailwind.css"],
-    }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    liveCode(),
-    react(),
-    sitemap(),
-  ],
+  integrations: [starlight({
+    title: "pbthang",
+    social: {
+      email: "mailto:bathang02@gmail.com",
+      github: "https://github.com/pbthang",
+      linkedin: "https://www.linkedin.com/in/pbthang/"
+    },
+    favicon: "/favicon.svg",
+    head: [{
+      tag: "link",
+      attrs: {
+        rel: "sitemap",
+        href: "/sitemap-index.xml"
+      }
+    }],
+    sidebar: [{
+      label: "Components",
+      items: [{
+        label: "Overview",
+        link: "/components"
+      }, {
+        label: "Hero",
+        autogenerate: {
+          directory: "/components/hero"
+        }
+      }, {
+        label: "Shadcn/ui",
+        autogenerate: {
+          directory: "/components/shadcn"
+        }
+      }]
+    }],
+    customCss: ["./src/tailwind.css"]
+  }), tailwind({
+    applyBaseStyles: false
+  }), liveCode(), react(), sitemap(), robotsTxt()],
   site: "https://pbthang-collection.netlify.app/",
   output: "server",
   adapter: netlify({
-    edgeMiddleware: true,
-  }),
+    edgeMiddleware: true
+  })
 });
