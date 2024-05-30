8bcaff238822db4547b9cef48ef3cfd540f9a47e
Thang Pham <bathang02@gmail.com>
2024-05-30 14:19:26 +0800

    add edit link

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,45 +10,64 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "pbthang",
-    social: {
-      email: "mailto:bathang02@gmail.com",
-      github: "https://github.com/pbthang",
-      linkedin: "https://www.linkedin.com/in/pbthang/"
-    },
-    favicon: "/favicon.svg",
-    head: [{
-      tag: "link",
-      attrs: {
-        rel: "sitemap",
-        href: "/sitemap-index.xml"
-      }
-    }],
-    sidebar: [{
-      label: "Components",
-      items: [{
-        label: "Overview",
-        link: "/components"
-      }, {
-        label: "Hero",
-        autogenerate: {
-          directory: "/components/hero"
-        }
-      }, {
-        label: "Shadcn/ui",
-        autogenerate: {
-          directory: "/components/shadcn"
-        }
-      }]
-    }],
-    customCss: ["./src/tailwind.css"]
-  }), tailwind({
-    applyBaseStyles: false
-  }), liveCode(), react(), sitemap(), robotsTxt()],
+  integrations: [
+    starlight({
+      title: "pbthang",
+      social: {
+        email: "mailto:bathang02@gmail.com",
+        github: "https://github.com/pbthang",
+        linkedin: "https://www.linkedin.com/in/pbthang/",
+      },
+      favicon: "/favicon.svg",
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "sitemap",
+            href: "/sitemap-index.xml",
+          },
+        },
+      ],
+      sidebar: [
+        {
+          label: "Components",
+          items: [
+            {
+              label: "Overview",
+              link: "/components",
+            },
+            {
+              label: "Hero",
+              autogenerate: {
+                directory: "/components/hero",
+              },
+            },
+            {
+              label: "Shadcn/ui",
+              autogenerate: {
+                directory: "/components/shadcn",
+              },
+            },
+          ],
+        },
+      ],
+      editLink: {
+        baseUrl:
+          "https://github.com/pbthang/component-collection/edit/edit-page",
+      },
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    liveCode(),
+    react(),
+    sitemap(),
+    robotsTxt(),
+  ],
   site: "https://pbthang-collection.netlify.app/",
   output: "server",
   adapter: netlify({
-    edgeMiddleware: true
-  })
+    edgeMiddleware: true,
+  }),
 });
